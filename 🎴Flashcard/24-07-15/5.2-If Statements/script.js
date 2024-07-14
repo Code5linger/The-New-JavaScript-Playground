a504f4c5af257818febbe9e42b56b0baef890cef
Code5linger <sakib.ahmed.1708@gmail.com>
2024-07-15 00:30:02 +0600

    5.2 - If Statements

---
+++ 🎴Flashcard/24-07-15/5.2-If Statements/script.js
@@ -0,0 +1,31 @@
+// if (true) {
+//   console.log('This is true');
+// }
+
+// if (false) {
+//   console.log('This is true');
+// }
+
+let x, y;
+
+x = 10;
+y = 5;
+if (x > y) console.log('X > Y');
+else console.log('🤖'), console.log('Naaah!');
+
+if (x === y) console.log('X === Y');
+else console.log('🤖'), console.log('Naaah!');
+
+if (x < y) console.log('X < Y');
+else console.log('🤖'), console.log('Naaah!');
+
+// if (true) {
+//   let x = 'Let';
+//   const y = 'Const';
+//   var z = 'Var';
+
+//   console.log(x, y, z);
+// }
+// // console.log(x);
+// // console.log(y);
+// console.log(z);
