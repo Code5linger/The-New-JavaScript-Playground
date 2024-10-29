bbc2db5f6db76dfaecee05adcc0892c1e916d3ae
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2024-10-29 20:35:04 +0600

    !

---
+++ 🎴Flashcard/24-10-29/JS➖0️⃣2️⃣➖3️⃣➖Using➖the➖Console.js
@@ -0,0 +1,34 @@
+// // alert('Yo!');
+
+// console.log(2 + 2);
+
+// let obj = { type: 'Fiat', model: '500', color: 'white' };
+
+// console.log(
+//   typeof 'String',
+//   typeof 69,
+//   typeof true,
+//   typeof undefined,
+//   typeof null,
+//   typeof ['a', 'b'],
+//   typeof obj
+// );
+
+console.groupCollapsed('🔻');
+console.error('Error');
+
+let x = 100;
+console.log(x);
+
+console.warn('Warn');
+console.warn('Warn');
+console.warn('Warn');
+console.warn('Warn');
+console.warn('Warn');
+
+console.groupEnd('🔼');
+console.table({ firstName: 'Elrick', lastName: 'Ashford' });
+
+const style = 'background: crimson; padding: 10px; border: 2px solid white';
+
+console.log('%cHello World!', style);
