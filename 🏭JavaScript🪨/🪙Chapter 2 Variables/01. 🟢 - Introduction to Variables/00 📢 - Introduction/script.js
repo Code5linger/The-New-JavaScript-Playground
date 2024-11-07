cda9d5d123cb63429850d56f40e5ebd05ab3c3dd
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2024-11-08 03:39:01 +0600

    !

---
+++ 🏭JavaScript🪨/🪙Chapter 2 Variables/01. 🟢 - Introduction to Variables/00 📢 - Introduction/script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const score = 0;
+console.log(score);
+
+let message = 'T_T';
+// alert(message);
+message = 'XD';
+// alert(message);
+
+console.log('Sup!');
+
+const suit = '🤵';
+console.log(suit);
+
+const myName = `Sakib`;
+console.log(myName);
+
+const birthDate = new Date('August 26, 1994');
+console.log(birthDate);
+
+// x = 69;
+
+if (5 == '5') {
+  console.log('?');
+} else {
+  console.log('??');
+}
+
+function fun() {
+  return 69;
+}
+
+console.log(fun());
+
+let a = 123;
+console.log(a);
+
+while (a > 200) {
+  a = 456;
+  foo(a / 2);
+}
+
+console.log(a);
+
+if (true) {
+  a = 69;
+  //   bar(a / 2);
+}
+console.log(a);
+
+function one() {}
+const four = function () {
+  return 40;
+};
+const six = function three() {
+  return 63;
+};
+
+console.log(six());
+console.log(four());
+// console.log(three());
