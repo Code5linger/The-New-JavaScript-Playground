6cd186d84e2ab98bd90927d8a512580606f5c3ba
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2024-11-08 22:15:46 +0600

    !

--- 🏭JavaScript🪨/🪙Chapter 2 Variables/01. 🟢 - Introduction to Variables/00 📢 - Introduction/script.js
+++ 🏭JavaScript🪨/🪙Chapter 2 Variables/01. 🟢 - Introduction to Variables/00 📢 - Introduction/script.js
@@ -60,3 +60,68 @@ const six = function three() {
 console.log(six());
 console.log(four());
 // console.log(three());
+
+const greetings = `Hello!`;
+console.log(greetings);
+greetings.toUpperCase(); // Method
+console.log(greetings.toLowerCase());
+console.log(greetings.toUpperCase());
+
+const tweet = `Hay Elon, lol cringe!`;
+console.log(tweet);
+console.log(tweet.indexOf(`Elon`));
+console.log(tweet.indexOf(`Hay`));
+console.log(tweet.indexOf('Test'));
+console.log(tweet.slice(14, 17));
+console.log(tweet.length);
+
+console.log(`Hello World!`.length);
+console.log(`Hello World!`.charAt(11));
+console.log(`Hello World!`.charAt(`Hello World!`.length - 1));
+console.log(tweet.slice(14).toUpperCase().slice(5).toLowerCase());
+
+const mail = `codeslinger@gmail.com`;
+
+const result = mail.lastIndexOf('.');
+console.log(result);
+
+console.log(mail.slice(0, 11));
+
+console.log(mail.replace('@', '#'));
+
+const dev = `developer`;
+console.log(dev.slice(0, 1).toUpperCase() + dev.slice(1, 20));
+console.log(dev.charAt(0).toUpperCase() + dev.substring(1));
+console.log(dev[0].toUpperCase() + dev.substring(1));
+
+let x;
+
+const name = `Sakib`,
+  age = `30`;
+
+x = 'Hello!';
+
+console.log(`${x}, My name is ${name}. I am ${age} `);
+
+console.log(x.length);
+
+x = typeof name;
+console.log(x);
+
+console.log(x.__proto__);
+
+const title = `Lord of the Rings`,
+  published = new Date(`1954-07-29`);
+
+const summary = `${title} is a book published in ${published.getFullYear()}`;
+
+console.log(summary);
+
+// console.log(published.__proto__);
+
+const _myName = `Sakib`,
+  email = `sakib@mail.com`,
+  myTitle = `Programmer`;
+
+const intro = `Hi! I am ${_myName}. My mail is ${email}. I am a ${myTitle}`;
+console.log(intro);
