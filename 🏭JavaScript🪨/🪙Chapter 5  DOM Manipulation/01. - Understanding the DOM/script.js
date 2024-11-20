fceed4fd871fae13b940e47d7482157bc3d5d779
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2024-11-20 08:35:15 +0600

    008 Adding & Removing Classes

--- 🏭JavaScript🪨/🪙Chapter 5  DOM Manipulation/01. - Understanding the DOM/script.js
+++ 🏭JavaScript🪨/🪙Chapter 5  DOM Manipulation/01. - Understanding the DOM/script.js
@@ -120,7 +120,6 @@ const main = document.getElementById('main');
 document.querySelector(`#main p`).innerHTML = `Sup!`;
 
 console.log(`0074_Traversing_The_DOM_All_Nodes `);
-let outpu;
 
 const parent = document.querySelector('.parent');
 
@@ -133,22 +132,24 @@ const parent = document.querySelector('.parent');
 // outpu = parent.childNodes[3].innerText = 'OnE';
 // outpu = parent.childNodes[5].style.color = 'red';
 
-output = parent.firstChild;
-output = parent.lastChild = `HeLlO`;
+// let outpu;
 
-const child = document.querySelector('#child');
+// output = parent.firstChild;
+// output = parent.lastChild = `HeLlO`;
 
-output = child.parentNode;
-output = child.parentElement;
+// const child = document.querySelector('#child');
+
+// output = child.parentNode;
+// output = child.parentElement;
 
 // child.parentNode.style.backgroundColor = `#ccc`;
-child.parentNode.style.padding = `1rem`;
+// child.parentNode.style.padding = `1rem`;
 
 const secondItem = document.querySelector('#child:nth-child(2)');
 
 // output = secondItem.nextSibling.style.color = 'red';
 
-console.log(output);
+// console.log(output);
 
 // # 2. **Selecting DOM Elements**
 //    - **Methods of Selection**
@@ -173,9 +174,9 @@ para = document.querySelectorAll('p');
 
 // console.log(para[0].innerHTML);
 
-para.forEach((par) => {
-  console.log(par);
-});
+// para.forEach((par) => {
+//   console.log(par);
+// });
 
 para = document.querySelectorAll('.error');
 
@@ -203,9 +204,9 @@ errors.forEach((err) => {
 let parag = document.getElementsByTagName(`p`);
 console.log(parag);
 
-parag.forEach((paras) => {
-  console.log(paras);
-});
+// parag.forEach((paras) => {
+//   console.log(paras);
+// });
 
 console.log(`0070_Document_Element_Properties `);
 
@@ -227,11 +228,11 @@ output = document.URL;
 output = document.characterSet;
 output = document.contentType;
 output = document.forms;
-output = document.forms[0].id;
-output = document.forms[0].method;
-output = document.forms[0].action;
+// output = document.forms[0].id;
+// output = document.forms[0].method;
+// output = document.forms[0].action;
 
-document.forms[0].id = 'new-id';
+// document.forms[0].id = 'new-id';
 output = document.links[0];
 output = document.links[0].href;
 output = document.links[0].href = 'facebook.com';
@@ -242,7 +243,7 @@ output = document.links[0].classList;
 
 output = document.images;
 output = document.images[0];
-output = document.images[0].src;
+// output = document.images[0].src;
 
 console.log(output);
 
@@ -263,55 +264,55 @@ forms.forEach((form) => {
 console.log(`0071_DOM_Selectors_Single_Elements `);
 
 console.log(document.getElementById(`app-id`));
-console.log(document.getElementById(`app-id`).id);
-console.log(document.getElementById(`app-id`).className);
-console.log(document.getElementById(`app-id`).getAttribute(`id`));
+// console.log(document.getElementById(`app-id`).id);
+// console.log(document.getElementById(`app-id`).className);
+// console.log(document.getElementById(`app-id`).getAttribute(`id`));
 
 // Set Attributes
 
-document.getElementById(`app-id`).id = `new-id`;
-document.getElementById(`new-id`).title = `New List`;
-document.getElementById(`new-id`).innerText = `New List`;
-console.log(document.getElementById(`new-id`).title);
+// document.getElementById(`app-id`).id = `new-id`;
+// document.getElementById(`new-id`).title = `New List`;
+// document.getElementById(`new-id`).innerText = `New List`;
+// console.log(document.getElementById(`new-id`).title);
 
-document.getElementById(`new-id`).setAttribute('class', 'o_O');
+// document.getElementById(`new-id`).setAttribute('class', 'o_O');
 
-const title = document.getElementById(`new-id`);
-console.log(title);
-console.log(title.textContent);
-title.textContent = 'O_O';
-title.innerText = `Hello Again!`;
-title.innerHTML = `<strong>Sup!</strong>`;
+// const titleAlt = document.getElementById(`new-id`);
+// console.log(titleAlt);
+// console.log(titleAlt.textContent);
+// titleAlt.textContent = 'O_O';
+// titleAlt.innerText = `Hello Again!`;
+// titleAlt.innerHTML = `<strong>Sup!</strong>`;
 
 // CHange Stype
 
-title.style.color = `crimson`;
-title.style.background = `black`;
-title.style.padding = `1rem `;
-title.style.border = `.25rem solid crimson`;
-title.style.borderRadius = `1rem`;
+// titleAlt.style.color = `crimson`;
+// titleAlt.style.background = `black`;
+// titleAlt.style.padding = `1rem `;
+// titleAlt.style.border = `.25rem solid crimson`;
+// titleAlt.style.borderRadius = `1rem`;
 
 // querySelector
-console.log(document.querySelector(`h1`));
-console.log(document.querySelector(`#new-id`));
-console.log(document.querySelector(`.container`));
-console.log(document.querySelector(`input[type='text']`));
-console.log(document.querySelector(`li:nth-child(2)`));
+// console.log(document.querySelector(`h1`));
+// console.log(document.querySelector(`#new-id`));
+// console.log(document.querySelector(`.container`));
+// console.log(document.querySelector(`input[type='text']`));
+// console.log(document.querySelector(`li:nth-child(2)`));
 
-const secondItem = document.querySelector(`li:nth-child(2)`);
-secondItem.innerText = 'Apple Juice';
-secondItem.style.color = `Green`;
+// const secondItemAlt = document.querySelector(`li:nth-child(2)`);
+// secondItemAlt.innerText = 'Apple Juice';
+// secondItemAlt.style.color = `Green`;
 
 // USe
 
-const list = document.querySelector(`ul`);
-console.log(list);
+// const list = document.querySelector(`ul`);
+// console.log(list);
 
-const firstItem = list.querySelector(`li`);
-firstItem.style.color = `blue`;
+// const firstItem = list.querySelector(`li`);
+// firstItem.style.color = `blue`;
 
-const thirdItem = list.querySelector(`li:nth-child(3)`);
-thirdItem.style.color = `white`;
+// const thirdItem = list.querySelector(`li:nth-child(3)`);
+// thirdItem.style.color = `white`;
 
 // const listArray = Array.from(list);
 
@@ -321,3 +322,108 @@ thirdItem.style.color = `white`;
 // });
 
 console.log(`005 Adding & Changing Page Content`);
+
+let paragraph = document.querySelector('p');
+
+console.log(paragraph.innerText);
+
+let paragraphs = document.querySelectorAll(`p`);
+
+console.log(paragraphs);
+
+// let paragraphArray = Array.from(paragraph);
+
+// console.log(paragraphArray);
+
+paragraphs.forEach((item) => {
+  console.log(item.innerText);
+  // item.style.background = `red`;
+  item.innerText += ` **`;
+});
+
+paragraph.innerText = `Ninjas are awesome!`;
+
+const contentAlt = document.querySelector(`.content`);
+
+console.log(contentAlt.innerHTML);
+
+// contentAlt.innerHTML = `<h1>THIS WHERE WE HOLD THEM</h1>`;
+contentAlt.innerHTML += `<h1>THIS WHERE WE HOLD THEM</h1>`;
+contentAlt.style.background = `#702f18`;
+
+const people = ['mario', 'luigi', `yoshi`];
+
+people.forEach((item) => {
+  console.log((contentAlt.innerHTML += `<p>${item} was here!</p>`));
+});
+
+console.log(`006 Getting & Setting Attributes`);
+
+const link = document.querySelector(`a`);
+
+console.log(link);
+console.log(link.getAttribute(`href`));
+
+link.setAttribute(`href`, `http://google.com`);
+
+console.log(link);
+console.log(link.getAttribute(`href`));
+
+link.innerText = `o_O`;
+
+const message = document.querySelector(`p.errorAlt`);
+
+// console.log(message.getAttribute(`class`));
+// message.setAttribute(`class`, `alt`);
+// message.setAttribute(`style`, `color: white`);
+
+const headline = document.querySelector(`h1`);
+
+console.log(`007 Changing CSS Styles`);
+
+// headline.setAttribute(`style`, `margin: 3rem`);
+
+console.log(headline.style);
+console.log(headline.style.color);
+
+// headline.style.margin = `.5rem`;
+// headline.style.padding = `3rem`;
+// headline.style.border = `.5rem solid black`;
+// headline.style.textAlign = `center`;
+
+// headline.style.padding = ``;
+
+console.log(`008 Adding & Removing Classes`);
+
+const result = document.querySelector(`p.errorAlt`);
+
+// result.setAttribute(`class`, `error`);
+
+console.log(result.classList);
+
+result.classList.add(`error`);
+result.classList.remove(`error`);
+
+result.classList.add(`success`);
+result.classList.remove(`success`);
+
+const challengeField = document.querySelectorAll(`.challange p`);
+
+// console.log(challengeField);
+
+challengeField.forEach((item) => {
+  if (item.innerText.includes('error')) {
+    item.classList.add(`error`, `output`);
+  } else if (item.innerText.includes(`success`)) {
+    item.classList.add(`success`, `output`);
+  } else {
+    item.classList.add(`output`);
+  }
+});
+
+const titleAlt = document.querySelector(`#title`);
+
+titleAlt.classList.toggle(`O_o`);
+titleAlt.classList.toggle(`O_o`);
+
+console.log(titleAlt);
