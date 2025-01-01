3a5db57e4339faeab09490adfc5a25535efa119e
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-01-01 20:30:08 +0600

    01. 🎯 - Event Basics

---
+++ JavaScript/Chapter 5  DOM/4-Event Handling/02TypesofEvents.js
@@ -0,0 +1,14 @@
+/*
+#### 2. Types of Events
+   - Mouse Events
+     - `click`, `dblclick`, `mousedown`, `mouseup`, `mousemove`, `mouseover`, `mouseout`, `contextmenu`
+   - Keyboard Events
+     - `keydown`, `keyup`, `keypress`
+   - Form Events
+     - `input`, `change`, `submit`, `focus`, `blur`
+   - Window Events
+     - `load`, `unload`, `scroll`, `resize`, `error`
+   - Touch Events
+     - `touchstart`, `touchmove`, `touchend`, and `touchcancel` (for mobile devices)
+*/
+
