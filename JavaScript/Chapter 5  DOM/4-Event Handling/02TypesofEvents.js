0c0570617a8267687738cbb2d859f3fb6ba5d66c
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-01-01 22:33:10 +0600

    013 More DOM Events

--- JavaScript/Chapter 5  DOM/4-Event Handling/02TypesofEvents.js
+++ JavaScript/Chapter 5  DOM/4-Event Handling/02TypesofEvents.js
@@ -1,7 +1,6 @@
 /*
 #### 2. Types of Events
-   - Mouse Events
-     - `click`, `dblclick`, `mousedown`, `mouseup`, `mousemove`, `mouseover`, `mouseout`, `contextmenu`
+
    - Keyboard Events
      - `keydown`, `keyup`, `keypress`
    - Form Events
@@ -12,3 +11,22 @@
      - `touchstart`, `touchmove`, `touchend`, and `touchcancel` (for mobile devices)
 */
 
+//! Mouse Events
+- `click`, `dblclick`, `mousedown`, `mouseup`, `mousemove`, `mouseover`, `mouseout`, `contextmenu`
+ 
+//* Create 10 p tags, 1st one with class copyMe 2nd one with box.
+
+//* Trigger an alert if someone is trying to copy the 1st p
+const copy = document.querySelector(`.copyMe`)
+
+copy.addEventListener(`copy`, () => alert(`Copying not allowed!`))
+
+//* Replace the p with box class with mouse pointer x & y offset 
+const box = document.querySelector(`.box`)
+
+box.addEventListener(`mousemove`, e => box.textContent = `X = ${e.offsetX}, Y = ${e.offsetY}`)
+
+//* Log the mouse wheel scroll x & y
+document.addEventListener(`wheel`, e => console.log(`X = ${e.pageX}, Y = ${e.pageY}`))
+
+//* 
