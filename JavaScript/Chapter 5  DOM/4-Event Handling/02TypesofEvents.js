5220ecf15fd3e9173ac6856a7e6866f75c34d147
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-01-02 23:54:46 +0600

    014 Building a Popup

--- JavaScript/Chapter 5  DOM/4-Event Handling/02TypesofEvents.js
+++ JavaScript/Chapter 5  DOM/4-Event Handling/02TypesofEvents.js
@@ -29,4 +29,20 @@ box.addEventListener(`mousemove`, e => box.textContent = `X = ${e.offsetX}, Y = 
 //* Log the mouse wheel scroll x & y
 document.addEventListener(`wheel`, e => console.log(`X = ${e.pageX}, Y = ${e.pageY}`))
 
-//* 
+//*  Create a button, on click that open a popup & a close button that close it 
+const button = document.querySelector('#popup')
+const popup = document.querySelector('.popup-wrapper')
+const close = document.querySelector('.popup-close')
+
+button.addEventListener('click', () => {
+  popup.style.display = 'block'
+})
+
+close.addEventListener('click', () => {
+  popup.style.display = 'none'
+})
+
+//* [Add functionality] Clicking anywhere outside of popup closes it
+popup.addEventListener('click', () => {
+  popup.style.display = 'none'
+})
