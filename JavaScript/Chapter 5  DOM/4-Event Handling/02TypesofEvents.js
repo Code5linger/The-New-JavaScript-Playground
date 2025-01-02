5b27aead875119f7f8d41859f3dfbe6baa18f85a
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-01-03 02:22:42 +0600

    !

--- JavaScript/Chapter 5  DOM/4-Event Handling/02TypesofEvents.js
+++ JavaScript/Chapter 5  DOM/4-Event Handling/02TypesofEvents.js
@@ -53,7 +53,7 @@ popup.addEventListener('click', () => {
 const img = document.querySelector('#reddit-image')
 const clicked = () => console.log(`Clicked`)
 
-img.addEventListener(`click`, clicked)
+// img.addEventListener(`click`, clicked)
 
 //* Declare a variable that selects the image, create a function that log `Double Clickd`. While clicked on image it should call the function 
 // const doubleClicked = () => console.log(`Double Clicked`)
@@ -69,12 +69,12 @@ const doubleClicked = () => {
   }
 }
 
-img.addEventListener('dblclick', doubleClicked)
+// img.addEventListener('dblclick', doubleClicked)
 
 //* Log `Right Clicked` while right clicking over the img 
 const onRightClick = () => console.log('Right Click')
 
-img.addEventListener('contextmenu', onRightClick)
+// img.addEventListener('contextmenu', onRightClick)
 
 //* Now prevent the right click 
 const rightClickDisabled = () => {
@@ -84,44 +84,130 @@ const rightClickDisabled = () => {
   });
 }
 
-img.addEventListener('contextmenu', rightClickDisabled)
+// img.addEventListener('contextmenu', rightClickDisabled)
 
 //* Log a mouse down event 
 const mouseDown = () => console.log(`Mouse Down`)
 
-img.addEventListener('mousedown', mouseDown)
+// img.addEventListener('mousedown', mouseDown)
 
 //* Log a mouse up event 
 const mouseUp = () => console.log(`Mouse Up`)
 
-img.addEventListener('mouseup', mouseUp)
+// img.addEventListener('mouseup', mouseUp)
 
 //* Log a mouse wheel event 
 const mouseWheel = () => console.log(`Mouse Wheel`)
 
-img.addEventListener('wheel', mouseWheel)
+// img.addEventListener('wheel', mouseWheel)
 
 //* Log a mouse over event 
 const mouseOver = () => console.log(`Mouse Over`)
 
-img.addEventListener('mouseover', mouseOver)
+// img.addEventListener('mouseover', mouseOver)
 
 //* Log a mouse out event 
 const mouseOut = () => console.log(`Mouse Out`)
 
-img.addEventListener('mouseout', mouseOut)
+// img.addEventListener('mouseout', mouseOut)
 
 //* Log a drag event 
 const drag = () => console.log(`draged`)
 
-img.addEventListener('drag', drag)
+// img.addEventListener('drag', drag)
 
 //* Log when drag start 
 const dragstart = () => console.log(`drag start`)
 
-img.addEventListener('dragstart', dragstart)
+// img.addEventListener('dragstart', dragstart)
 
 //* Log when drag end
 const dragend = () => console.log(`drag end`)
 
-img.addEventListener('dragend', dragend)
+// img.addEventListener('dragend', dragend)
+
+//* Log the pointer Event
+// img.addEventListener('click', (e) => {
+//   console.log(e)
+// })
+
+//* Log the clicked element
+// function onClick(e) {
+//   console.log(e.target)
+// }
+
+// img.addEventListener('click', onClick)
+
+//* Log the element that the event listener is attached to
+// function onClick(e) {
+//   console.log(e.currentTarget)
+// }
+
+// img.addEventListener('click', onClick)
+
+//* Change the bg color of the clicked item 
+function onClick(e) {
+  e.target.style.backgroundColor = `crimson`
+}
+
+img.addEventListener('click', onClick)
+
+//* Add EventListener to the body. Log the clicked element & the element that the event listener is attached to
+// document.body.addEventListener('click', (e) => {
+//   console.log(e.target)
+//   console.log(e.currentTarget)
+// })
+
+//* Log the type of event that was triggered
+// function onClick(e) {
+//   console.log(e.type)
+// }
+
+// img.addEventListener('click', onClick)
+//* Log when the type of event that was triggered
+// function onClick(e) {
+//   console.log(e.timeStamp)
+// }
+
+// img.addEventListener('click', onClick)
+
+
+//* Log the X & Y position of the mouse click relative to the window
+// function onClick(e) {
+//   console.log(e.clientX, e.clientY)
+// }
+
+// img.addEventListener('click', onClick)
+
+//* Log the X & Y position of the mouse click relative to the element
+function onClick(e) {
+  console.log(e.offsetX, e.offsetY)
+}
+
+// img.addEventListener('click', onClick)
+
+//* Log the X & Y position of the mouse click relative to the page
+function onClick(e) {
+  console.log(e.pageX, e.pageY)
+}
+
+// img.addEventListener('click', onClick)
+
+//* Log the X & Y position of the mouse click relative to the screen
+function onClick(e) {
+  console.log(e.screenX, e.screenY)
+}
+
+// img.addEventListener('click', onClick)
+
+//* Add a link in html. Prevent it's default behavious & log `Link was clicked!`
+document.querySelector('#link').addEventListener('click', function(e) {
+  e.preventDefault();
+  console.log(`Link was clicked!`)
+})
+
+//* Print drag x & y tracking position to the page 
+const ondrag = (e) => {
+  document.querySelector(`#link`).textContent = `X ${e.clientX} Y ${e.clientY}`
+}
+img.addEventListener('drag', ondrag)
