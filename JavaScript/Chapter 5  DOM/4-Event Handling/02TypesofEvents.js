e861584c814fa2cc3a2c5a98c7960714794e449b
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-01-03 01:16:11 +0600

    0085_Mouse_Events

--- JavaScript/Chapter 5  DOM/4-Event Handling/02TypesofEvents.js
+++ JavaScript/Chapter 5  DOM/4-Event Handling/02TypesofEvents.js
@@ -27,7 +27,7 @@ const box = document.querySelector(`.box`)
 box.addEventListener(`mousemove`, e => box.textContent = `X = ${e.offsetX}, Y = ${e.offsetY}`)
 
 //* Log the mouse wheel scroll x & y
-document.addEventListener(`wheel`, e => console.log(`X = ${e.pageX}, Y = ${e.pageY}`))
+// document.addEventListener(`wheel`, e => console.log(`X = ${e.pageX}, Y = ${e.pageY}`))
 
 //*  Create a button, on click that open a popup & a close button that close it 
 const button = document.querySelector('#popup')
@@ -46,3 +46,82 @@ close.addEventListener('click', () => {
 popup.addEventListener('click', () => {
   popup.style.display = 'none'
 })
+
+//* Add an image to the html
+
+//* Declare a variable that selects the image, create a function that log `Clickd`. While clicked on image it should call the function 
+const img = document.querySelector('#reddit-image')
+const clicked = () => console.log(`Clicked`)
+
+img.addEventListener(`click`, clicked)
+
+//* Declare a variable that selects the image, create a function that log `Double Clickd`. While clicked on image it should call the function 
+// const doubleClicked = () => console.log(`Double Clicked`)
+
+// img.addEventListener('dblclick', doubleClicked)
+
+//* Rather than logging a text, change the background to yellow when double clicked than turn it back when double clicked again
+const doubleClicked = () => {
+  if (document.body.style.backgroundColor !== 'yellow') {
+    document.body.style.backgroundColor = 'yellow'
+  } else {
+    document.body.style.backgroundColor = 'black'
+  }
+}
+
+img.addEventListener('dblclick', doubleClicked)
+
+//* Log `Right Clicked` while right clicking over the img 
+const onRightClick = () => console.log('Right Click')
+
+img.addEventListener('contextmenu', onRightClick)
+
+//* Now prevent the right click 
+const rightClickDisabled = () => {
+  document.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+    console.log('Right-click is disabled on this page.');
+  });
+}
+
+img.addEventListener('contextmenu', rightClickDisabled)
+
+//* Log a mouse down event 
+const mouseDown = () => console.log(`Mouse Down`)
+
+img.addEventListener('mousedown', mouseDown)
+
+//* Log a mouse up event 
+const mouseUp = () => console.log(`Mouse Up`)
+
+img.addEventListener('mouseup', mouseUp)
+
+//* Log a mouse wheel event 
+const mouseWheel = () => console.log(`Mouse Wheel`)
+
+img.addEventListener('wheel', mouseWheel)
+
+//* Log a mouse over event 
+const mouseOver = () => console.log(`Mouse Over`)
+
+img.addEventListener('mouseover', mouseOver)
+
+//* Log a mouse out event 
+const mouseOut = () => console.log(`Mouse Out`)
+
+img.addEventListener('mouseout', mouseOut)
+
+//* Log a drag event 
+const drag = () => console.log(`draged`)
+
+img.addEventListener('drag', drag)
+
+//* Log when drag start 
+const dragstart = () => console.log(`drag start`)
+
+img.addEventListener('dragstart', dragstart)
+
+//* Log when drag end
+const dragend = () => console.log(`drag end`)
+
+img.addEventListener('dragend', dragend)
