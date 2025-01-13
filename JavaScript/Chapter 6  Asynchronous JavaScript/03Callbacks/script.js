c88c41f8ab841b67d56ac397c67a0c6c41af60af
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-01-13 23:24:25 +0600

    🎴 003 What are HTTP Requests_ ⌚6. 45

--- JavaScript/Chapter 6  Asynchronous JavaScript/03Callbacks/script.js
+++ JavaScript/Chapter 6  Asynchronous JavaScript/03Callbacks/script.js
@@ -9,4 +9,6 @@ setTimeout( () => console.log('callback function fired'), 2000)
 console.log(3)
 console.log(4)
 
+//* Go to `https://jsonplaceholder.typicode.com/` and run the demo script on the site. Than copy the link and visit it
+
 //* 
