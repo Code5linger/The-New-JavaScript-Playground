f2122e3859ea2667d953169ddaa0580ebe18269f
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-01-13 23:50:28 +0600

    🎴 004 Making HTTP Requests (XHR) ⌚ 7.5

--- JavaScript/Chapter 6  Asynchronous JavaScript/03Callbacks/script.js
+++ JavaScript/Chapter 6  Asynchronous JavaScript/03Callbacks/script.js
@@ -11,4 +11,22 @@ console.log(4)
 
 //* Go to `https://jsonplaceholder.typicode.com/` and run the demo script on the site. Than copy the link and visit it
 
-//* 
+//* A Create a new Request Object 
+const request = new XMLHttpRequest()
+
+//* C In the console's network section we can see we got the response. Now show it in the window. But first log the states of the request
+// request.addEventListener('readystatechange', () => {
+//     console.log(request, request.readyState)
+// })
+
+//* D When the state is in `done` stage, use the state to log the response text  
+request.addEventListener('readystatechange', () => {
+    // console.log(request, request.readyState)
+    if (request.readyState === 4) {
+        console.log(request.responseText)
+    }
+})
+
+//* B Using the request object, get all the objects from `https://jsonplaceholder.typicode.com/`
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+request.send()
