2e0d6cacdfec05dd0c7b4184675e22f62ea85dff
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-01-13 23:18:52 +0600

    🎴 002 Async Code in Action

---
+++ JavaScript/Chapter 6  Asynchronous JavaScript/03Callbacks/script.js
@@ -0,0 +1,12 @@
+//* Write 4 console logs without any gap
+
+//* In the middle write a callback function that waits 2 sec
+console.log(1)
+console.log(2)
+
+setTimeout( () => console.log('callback function fired'), 2000)
+
+console.log(3)
+console.log(4)
+
+//* 
