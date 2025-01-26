8726621236d780346ae0cc82b1aaec7ac32756b1
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-01-26 16:18:02 +0600

    🎴 90 - Local Storage API ⌚1:07'

---
+++ JavaScript/🪙Chapter 7  Web Browser APIs/03. - Commonly Used Web APIs/01. - Web Storage API/script.js
@@ -0,0 +1,6 @@
+// * Insert Some velues inside the local storage, where key = 1C69, value = test = O_o 
+localStorage.setItem('1C69', { "Test": 'O_o', "Demo": 'Demo!' })
+
+//* Use the value from the local storage in the site ex: log it
+const localValue = localStorage.getItem('1C69')
+console.log(localValue);
