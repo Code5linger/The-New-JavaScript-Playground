d24259efe67b8c9bb708f6f824e28591205bb3b9
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-03-06 03:57:35 +0600

    08. Callbacks

--- JavaScript/02 - GSAP/2-Beyond The Docs/script.js
+++ JavaScript/02 - GSAP/2-Beyond The Docs/script.js
@@ -1,5 +1,41 @@
-gsap.to('.fred', { x:300, y:300, scale: 3, duration:2, onComplete: onComplete, onCompleteParams: ['Test', 69]})
+// gsap.to(".fred", { scale: 3, duration: 2, onComplete: finish, onCompleteParams:["Tween Parameter!", 69] })
 
-function onComplete(msg, num) {
-    console.log(msg, num);
+// function finish(parameter, number) {
+//     console.log(parameter, number);
+// }
+
+// Callback Scope
+// gsap.to(".fred", { scale: 3, onComplete: onCompleteFunction })
+
+// function onCompleteFunction() {
+//     console.log(this.duration());
+// }
+
+// Arrow function callbacks
+// gsap.to(".fred", { scale: 3, onComplete: () => demo })
+
+// function onCompleteFunction() {
+//     console.log(this.targets()[0]);
+// }
+
+// const demo = () => console.log(this.duration())
+
+// demo()
+
+// Changing Callback Scope
+class Fred{
+    constructor() {
+        this.animation = gsap.to(".fred", {
+            scale: 4,
+            onComplete: onCompleteFunction,
+            callbackScope: this
+        })
+        this.message = "I am Fred!"
+
+        function onCompleteFunction() {
+            console.log(this.animation.duration());
+        }
+    }
 }
+
+let demo = new Fred()
