2b403805f694475265374289cf89038a1f33123b
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-03-06 06:38:18 +0600

    08. Callbacks

--- JavaScript/02 - GSAP/2-Beyond The Docs/script.js
+++ JavaScript/02 - GSAP/2-Beyond The Docs/script.js
@@ -23,18 +23,102 @@
 // demo()
 
 // Changing Callback Scope
+// class Fred{
+//     constructor() {
+//         this.animation = gsap.to(".fred", {
+//             scale: 4,
+//             onComplete: onCompleteFunction,
+//             callbackScope: this
+//         })
+//         this.message = "I am Fred!"
+
+//         function onCompleteFunction() {
+//             console.log(this.animation.duration());
+//         }
+//     }
+// }
+
+// let demo = new Fred()
+
+// Callbacks
+
+// Pass parameters into a callback
+// function onCompleteFunction(parameterText, parameterNumber) {
+//     console.log(parameterText, parameterNumber);
+// }
+
+// gsap.to('.fred', {
+//     delay: 1,
+//     duration: 2,
+//     x: 250,
+//     y: 250,
+//     scale: 3,
+//     onComplete: onCompleteFunction,
+//     onCompleteParams: ["Passed from GSAP!", 69],
+// })
+
+
+// Explain callback scope
+// function onCompleteFunction() {
+//     console.log(this.targets()[0]);
+// }
+
+// gsap.to(".fred", {
+//     delay: 1,
+//     duration: 2,
+//     x: 250,
+//     y: 250,
+//     scale: 3,
+//     onComplete: onCompleteFunction,
+// });
+
+// A friendly warning about using arrow functions
+
+// Reference an animation from a callback
+// gsap.to(".fred", {
+//     delay: 1,
+//     duration: 2,
+//     x: 250,
+//     y: 250,
+//     scale: 3,
+//     onComplete: () => console.log(this.duration()),
+// });
+
+// Change callback scope
 class Fred{
     constructor() {
-        this.animation = gsap.to(".fred", {
-            scale: 4,
+        this.animation = gsap.fromTo(".fred", {}, {
+            delay: 1,
+            duration: 2,
+            x: 250,
+            y: 250,
+            scale: 3,
+            yoyo: true,
+            repeat: 2,
             onComplete: onCompleteFunction,
+            onRepeat: onRepeatFunction,
+            onReverseComplete: onReverseCompleteFunction,
+            onStart: onStartFunction,
             callbackScope: this
-        })
-        this.message = "I am Fred!"
+        });
+        this.message = 'I am Fred!'
+        
 
         function onCompleteFunction() {
             console.log(this.animation.duration());
         }
+        function onRepeatFunction() {
+            console.log('Animation Repeated!');
+        }
+
+        function  onReverseCompleteFunction() {
+        console.log("Reverse animation completed!");
+        }
+
+        function onStartFunction() {
+            console.log('Animation Started!');
+        }
+
     }
 }
 
