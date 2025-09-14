1ddac60010e0a3091d4c63f0999c4b1e4d761f3e
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-09-15 03:19:17 +0600

    !

--- firebae-tut/index.js
+++ firebae-tut/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: 'https://scrimba-6497b-default-rtdb.asia-southeast1.firebasedatabase.app/'
@@ -7,29 +7,64 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+const booksInDB = ref(database, "books")
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-const shoppingListEl = document.getElementById("shopping-list")
+const booksEl = document.getElementById("books")
 
-const formReset = () => {
-     inputFieldEl.value = ""
+function appendBookToBooksListEl(bookValue) {
+    booksEl.innerHTML += `<li>${bookValue}</li>`
 }
 
-const addValue = (shoppingListEl, inputValue) => {
-    const li = document.createElement("li")
-    li.textContent = inputValue
-    shoppingListEl.appendChild(li)
+function clearBooksListEl() {
+    booksEl.innerHTML = ""
 }
 
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
+onValue(booksInDB, function (snapshot) {
+    clearBooksListEl()
 
-    push( shoppingListInDB ,inputValue)
+    let bookArray = Object.values(snapshot.val())
 
-    addValue(shoppingListEl, inputValue)
-    
-    // Clear input field
-    formReset()
+    // bookArray.map(book => {
+    //     console.log(book);
+    // })
+
+    for (let i = 0; i < bookArray.length; i++) {
+        console.log(bookArray[i]);
+        appendBookToBooksListEl(bookArray[i])
+    }
+
+    console.log(bookArray)
 })
+
+
+
+
+
+// const app = initializeApp(appSettings)
+// const database = getDatabase(app)
+// const shoppingListInDB = ref(database, "shoppingList")
+
+// const inputFieldEl = document.getElementById("input-field")
+// const addButtonEl = document.getElementById("add-button")
+// const shoppingListEl = document.getElementById("shopping-list")
+
+// const formReset = () => {
+//      inputFieldEl.value = ""
+// }
+
+// const addValue = (shoppingListEl, inputValue) => {
+//     const li = document.createElement("li")
+//     li.textContent = inputValue
+//     shoppingListEl.appendChild(li)
+// }
+
+// addButtonEl.addEventListener("click", function() {
+//     let inputValue = inputFieldEl.value
+
+//     push( shoppingListInDB ,inputValue)
+
+//     addValue(shoppingListEl, inputValue)
+    
+//     // Clear input field
+//     formReset()
+// })
