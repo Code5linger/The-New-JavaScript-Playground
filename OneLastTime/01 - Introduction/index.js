54ffa310cf4c494a6446a53e117c0bcd6915f54b
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-11-28 19:47:53 +0600

    !

---
+++ OneLastTime/01 - Introduction/index.js
@@ -0,0 +1,43 @@
+// console.log('O_o');
+
+// let x = 69
+
+// console.log(x++); // 69
+// console.log(x); //70
+// console.log(x++); // 70
+// console.log(x++); // 71
+
+// console.log(++x); //70
+// console.log(++x); //71
+// console.log(++x); //72
+
+// x++
+// ++x
+
+// x = x + 1
+
+// console.log(x); //70
+// ++x // x = x + 1
+// console.log(x); //71
+
+// let y = '5'
+
+// console.log(typeof (y));
+
+// x = x + y // 70 + '5'
+
+// console.log(x); // 705
+// console.log(typeof(x));
+
+// console.log(++y);// 51? 6?
+
+// let h = parseInt('9A')
+    
+// console.log(++h);
+
+// let z = x + y
+// console.log(z);
+//
+// console.log(y++);
+// console.log(++y);
+
