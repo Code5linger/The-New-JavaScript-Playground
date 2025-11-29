df9f938844bc5087ca9225cda88b1edeed5aa6af
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-11-29 23:59:32 +0600

    !

--- OneLastTime/01 - Introduction/index.js
+++ OneLastTime/01 - Introduction/index.js
@@ -45,7 +45,7 @@
 
 var msg = 'Welcome to JS'
 
-console.log(msg);
+// console.log(msg);
 
 let firstName = 'Sakib'
 let lastName = 'Ahamed'
@@ -57,7 +57,7 @@ numYearsEmployed = 7
 
 var dog = '🐕'
 
-console.log(dog);
+// console.log(dog);
 
 var scopeTest = 10
 
@@ -66,8 +66,108 @@ function scopeTestFunc() {
     console.log('O_o', scopeTest);
 }   
 
-console.log(scopeTest);
-scopeTestFunc()
-console.log(scopeTest);
+// console.log(scopeTest);
+// scopeTestFunc()
+// console.log(scopeTest);
 
-// 
+//
+
+function upper(strings, ...values) { }
+
+let name = 'Shakib', x = 'shakib94', topic = 'JS'
+
+// console.log(`Hello, ${name} (@${x})! Wellcome to ${topic}!`);
+
+const obj = {
+    name: 'Shakib',
+    age: 31,
+    objFunc: () => {console.log(name)}
+}
+
+// let newObj = obj.objFunc()
+
+const blog = {
+    title: 'Title',
+    content: 'Content',
+    author: 'Author',
+    publish() { console.log(`Publish ${this.title}`) },
+    unpublish() { console.log('Unpublish', this.content) },
+    delete() { console.log('Delete', this.author) }
+}
+
+// let newBlogPost = blog.publish()
+// newBlogPost = blog.unpublish()
+// newBlogPost = blog.delete()
+
+// console.log(Object.getPrototypeOf(blog));
+// console.log(blog.__proto__);
+
+const myDate = new Date()
+myDate.getDate()
+
+// console.log(myDate);
+
+const library = [
+    {
+        title: 'TitleA',
+        author: 'AuthorA',
+        status: {
+            own: true,
+            reading: true,
+            read: true
+    
+        }
+    },{
+        title: 'TitleB',
+        author: 'AuthorB',
+        status: {
+            own: true,
+            reading: true,
+            read: true
+    
+        }
+    },{
+        title: 'TitleC',
+        author: 'AuthorC',
+        status: {
+            own: true,
+            reading: true,
+            read: true
+    
+        }
+    }
+]
+
+// console.log(library);
+
+library[0].status.read = false
+library[1].status.read = false
+library[2].status.read = false
+
+library[0].title = 'firstBook'
+
+const {title: firstBook} = library[0]
+
+// console.log(library);
+
+const libraryJSON = JSON.stringify(library)
+
+// console.log(libraryJSON);
+// console.log(library[0]["title"]);
+
+delete library[2].title
+// console.log(library);
+
+var students = [
+    {name: 'NameA', track: 'TrackA', achievement: 'O_oA', points: 67},
+    {name: 'NameB', track: 'TrackB', achievement: 'O_oB', points: 69},
+    {name: 'NameC', track: 'TrackC', achievement: 'O_oC', points: 111},
+    {name: 'NameD', track: 'TrackD', achievement: 'O_oD', points: 666},
+    {name: 'NameE', track: 'TrackE', achievement: 'O_oE', points: 707},
+]
+
+// students.map(student => console.log(`${student.name} ${student.track} ${student.achievement} ${student.points}`))
+
+for (let i = 0; i < students.length; i++) {
+    console.log(students[i].name + students[i].achievement + students[i].points);
+}
