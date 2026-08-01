501cd52e4f9d1c046a766f49ca7a6a3a952b34e5
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2026-08-01 20:23:15 +0600

    !

---
+++ script.js
@@ -0,0 +1,11 @@
+const name = 'Sakib';
+
+// Function
+const greet = () => 'Hello';
+
+let resultOne = greet();
+console.log(resultOne + ' ' + name);
+
+// Method
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
