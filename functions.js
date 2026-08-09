0af22c296b09c5cb74019b45dda7868d4158b503
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2026-08-10 02:36:17 +0600

    !

--- functions.js
+++ functions.js
@@ -1,15 +1,41 @@
-const RandomNumber = Math.floor(Math.random() * 6) + 1;
+const nemo = [
+  'Marlin',
+  'Dory',
+  'Gill',
+  'Bloat',
+  'Peach',
+  'Gurgle',
+  'Bubbles',
+  'Deb',
+  'Jacques',
+  'Nigel',
+  'Crush',
+  'Squirt',
+  'Bruce',
+  'Anchor',
+  'Chum',
+  'Mr. Ray',
+  'Coral',
+  'Darla',
+  'Philip Sherman',
+  'Pearl',
+  'Sheldon',
+  'Tad',
+  'nemo',
+];
 
-console.log(RandomNumber);
+const largeArray = new Array(100000).fill('nemo');
 
-function RandomNumberFunction() {
-  console.log(Math.floor(Math.random() * 6) + 1);
+function findNemo(array) {
+  let time0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    if (array[i] === 'nemo') {
+      console.log('Found Nemo!🔥');
+    }
+  }
+  let time1 = performance.now();
+  console.log('Finding Nemo took ' + Math.floor(time1 - time0) + '/ms');
 }
 
-RandomNumberFunction();
-RandomNumberFunction();
-RandomNumberFunction();
-RandomNumberFunction();
-RandomNumberFunction();
-RandomNumberFunction();
-
+findNemo(largeArray);
