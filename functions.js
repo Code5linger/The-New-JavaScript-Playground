4a22504cab5417dc9c2ae53d80143df3e538015f
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2026-08-09 19:19:29 +0600

    !

---
+++ functions.js
@@ -0,0 +1,15 @@
+const RandomNumber = Math.floor(Math.random() * 6) + 1;
+
+console.log(RandomNumber);
+
+function RandomNumberFunction() {
+  console.log(Math.floor(Math.random() * 6) + 1);
+}
+
+RandomNumberFunction();
+RandomNumberFunction();
+RandomNumberFunction();
+RandomNumberFunction();
+RandomNumberFunction();
+RandomNumberFunction();
+
