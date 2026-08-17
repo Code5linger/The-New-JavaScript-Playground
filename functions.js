9f7cc570119c1f51f3f3d88570a1b145342264ce
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2026-08-17 23:29:16 +0600

    !

---
+++ functions.js
@@ -0,0 +1,14 @@
+var count = 0;
+
+var dogCount = 0;
+var birdCount = 0;
+
+function countBird() {
+  count += 1;
+  return count + ' birds!';
+}
+
+function countDogs() {
+  count += 1;
+  return count + ' dogs!';
+}
